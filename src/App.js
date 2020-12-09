02b6b1f503f2d916009c2c0d70ef7f76e141ad6f
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-09 20:26:00 -0300

    feat: add header component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+/* Components */
 import Login from './pages/Login';
+import Header from './components/Header';
 
+/* Styles */
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyles />
     </>
