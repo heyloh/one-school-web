b0ae7a744a03e5a991f572a4d005dd664d618bd3
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-09 19:08:01 -0300

    feat: add global styles and styled container
    Because these are styles that we'll use throughout the application, we configure these globally to
    avoid repetition of code.

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,63 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import {
+  primaryColor,
+  primaryDarkColor,
+  primaryColorHover,
+} from '../config/colors';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Nunito', sans-serif;
+    background: #F3F3F3;
+    background: ${primaryDarkColor};
+    color: ${primaryDarkColor};
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  button {
+    cursor: pointer;
+    background: ${primaryColor};
+    border: none;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    width: 150px;
+    height: 46px;
+    font-size: 16px;
+    font-family: 'Nunito', sans-serif;
+    font-weight: bold;
+    transition: 0.2s;
+    :hover {
+      background: ${primaryColorHover};
+    }
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+`;
+
+export const Container = styled.section`
+  max-width: 360px;
+  background: #f3f3f3;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  margin: 30px auto;
+  padding: 30px;
+`;
