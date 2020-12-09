ad50cf31667b5d08d3b80503078c62061814969a
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-09 19:06:27 -0300

    feat: add website colors configuration

---
+++ src/config/colors.js
@@ -0,0 +1,9 @@
+export const primaryColor = '#7159C1';
+export const primaryColorHover = '#8F7AD6';
+export const primaryDarkColor = '#1A1A22';
+
+export const successColor = '#5FCE7E';
+export const infoColor = '#A0D8BD';
+
+export const errorColor = '#CE5F5F';
+export const warnColor = '#E8A4A4';
