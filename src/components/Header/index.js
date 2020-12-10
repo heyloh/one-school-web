9b291f0b87d29bcd0b2430fc5fe892f629ceaaae
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-09 21:09:08 -0300

    feat: add site logo and restructure header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 
+import logo from '../../images/logo.svg';
+
 import { Nav } from './styled';
 
 function Header() {
   return (
     <Nav>
-      <a href="/">
-        <FaHome size={24} color="#FFF" />
-      </a>
-      <a href="/">
-        <FaUserAlt size={24} color="#FFF" />
-      </a>
-      <a href="/">
-        <FaSignInAlt size={24} color="#FFF" />
-      </a>
+      <img src={logo} alt="schoolapp" />
+      <div>
+        <a href="/">
+          <FaHome size={20} color="#FFF" />
+        </a>
+        <a href="/">
+          <FaUserAlt size={20} color="#FFF" />
+        </a>
+        <a href="/">
+          <FaSignInAlt size={20} color="#FFF" />
+        </a>
+      </div>
     </Nav>
   );
 }
