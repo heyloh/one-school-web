c689bc3e00bd9670d7cb1c7779ca53381c175eae
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-09 21:58:21 -0300

    feat: add page 404 not found
    This page will appear whenever someone navigate to a page that doesn't exist on the website.

---
+++ src/pages/Page404/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+
+function Page404() {
+  return (
+    <Container>
+      <h1>404 PAGE NOT FOUND</h1>
+      <p>Hey, what are you doing here?</p>
+      <p>This page doesn&apos;t exist. Go back!</p>
+    </Container>
+  );
+}
+
+export default Page404;
