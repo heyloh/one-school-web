2943af94c787293d2be6d275807f0f7d40dbe0fd
Lohana Torres <lt.leopoldo@gmail.com>
2021-02-10 17:15:55 -0300

    feat: configure my route

---
+++ src/routes/MyRoute.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function MyRoute({ component: Component, isClosed, ...rest }) {
+  const isLoggedIn = false;
+
+  if (isClosed && !isLoggedIn) {
+    return (
+      <Redirect
+        to={{
+          pathname: '/login',
+          state: {
+            prevPath: rest.location.pathname,
+          },
+        }}
+      />
+    );
+  }
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Route {...rest} component={Component} />;
+}
+
+export default MyRoute;
+
+MyRoute.defaultProps = {
+  isClosed: false,
+};
+
+MyRoute.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+  isClosed: PropTypes.bool,
+};
